b46e1385a2882d99a11ca5e6c31b4e8fef8f0814
humonnom <juepark42@gmail.com>
2025-11-20 16:35:11 +0900

    Remove duplicated sentry initial setting

--- App.tsx
+++ App.tsx
@@ -20,25 +20,6 @@ import {toastConfig} from "./lib/toast.config";
 import type {MaterialIconName} from "./types";
 import * as Sentry from '@sentry/react-native';
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
-
-  // Enable Logs
-  enableLogs: true,
-
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
-});
-
 // Sentry 초기화
 initSentry();
 
